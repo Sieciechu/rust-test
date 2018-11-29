894eae4a5c801fb9ddefcca413f3e996a29a6774
Sieciech <wojciech.mocek@o2.pl>
2018-11-29 15:16:57 +0100

    implemented guessing

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# rust-test
+# Description
+Rust test. Simple number guessing game.
+
+# Github
 
 [![Build Status](https://travis-ci.com/Sieciechu/rust-test.png?branch=master)](https://travis-ci.org/Sieciechu/rust-test)
 [![HitCount](http://hits.dwyl.com/Sieciechu/rust-test.svg)](http://hits.dwyl.com/Sieciechu/rust-test)
 
+Run: `git clone git@github.com:Sieciechu/rust-test.git && cd rust-test && ./installandrun.sh`
 
 # Dockerhub
 https://hub.docker.com/r/sieciech/rust-test/
@@ -11,3 +15,4 @@ https://hub.docker.com/r/sieciech/rust-test/
 [![](https://images.microbadger.com/badges/version/sieciech/rust-test.svg)](https://microbadger.com/images/sieciech/rust-test "Get your own version badge on microbadger.com")
 [![](https://images.microbadger.com/badges/commit/sieciech/rust-test.svg)](https://microbadger.com/images/sieciech/rust-test "Get your own commit badge on microbadger.com")
 
+Run: `docker container run --rm -it sieciech/rust-test`
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number");
-
-    println!("Input the guess.");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {}", guess);
-
-    let x = 1;
-    let y = 6;
-    println!("x={:?}, y={}, x+y={}", guess, y, x+y );
+    const MIN: i32 = 1;
+    const MAX: i32 = 10;
     
+    println!("Guess the number from {} to {}.", MIN, MAX);
+
+    let secret_number = rand::thread_rng().gen_range(MIN,MAX+1);
+    println!("The secret number is {}\n\n", secret_number);
+
+    loop {
+        println!("Input the guess.");
+        let mut guess = String::new();
+        
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number");
+                continue;
+            }
+        };
+        
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+
+        }
+    }
 }
