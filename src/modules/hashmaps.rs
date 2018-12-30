0fb939e6cc3f7f9cff7e6bf56cd84c41b1e746a6
Sieciech <wojciech.mocek@o2.pl>
2018-12-30 14:08:25 +0100

    hashmaps part2

--- src/modules/hashmaps.rs
+++ src/modules/hashmaps.rs
@@ -26,13 +26,45 @@ pub fn run(){
     print_men(&men);
     eprintln!("men = {:#?}", men);
 
-    for (key, value) in men {
+    for (key, value) in &men {
         eprintln!("for loop, man named {} is of age {}", key, value);
     }
+
+
+    let not_existing_man = "Johnnn".to_string();
+    eprintln!("not_existing_man = {:#?}", not_existing_man);
+
+    let man = men.get(&not_existing_man);
+
+    eprintln!("{} is {:#?}", not_existing_man, man.unwrap_or(&12));
+    eprintln!("{} is {:#?}", not_existing_man, man.unwrap_or_else(|| &20));
+    let message = match man {
+        Some(i) => i.to_string(),
+        None => String::from("there no such guy like johnnnn"),
+    };
+    eprintln!("with assigned matched variable = {:#?}", message);
+
+    eprintln!("short form = {:#?}", match men.get(&not_existing_man) {
+        Some(i) => i.to_string(),
+        None => format!("there no such guy like {}", &not_existing_man),
+    });
+
+    count_words_in_string(&format!("Hello Mr! Are you ok? How are you Mr {}?", not_existing_man));
 }
 
 fn print_men(men: &std::collections::HashMap<String, u32>){
     for (i, (name, age)) in men.iter().enumerate() {
         eprintln!("man[{}] is named {} and his age is {}", i, name, age );
     }
+}
+
+fn count_words_in_string(text: &str){
+    let mut map = std::collections::HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word.to_lowercase()).or_insert(0);
+        *count +=1;
+    }
+
+    eprintln!("counted words = {:#?}", map);
 }
