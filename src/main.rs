08656436b782401f36c5d43f4887f81c709a9aef
Sieciech <wojciech.mocek@o2.pl>
2019-01-21 05:51:03 +0100

    tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::net::{TcpListener, TcpStream};
 
-mod modules;
-
-fn main() {
-    modules::strings::run();
-    modules::errors::run();
+fn handle_client(stream: TcpStream) {
+    eprintln!("stream = {:#?}", stream);
 }
 
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
