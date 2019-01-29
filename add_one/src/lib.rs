cb863cdc652517427fa59a0995ce7e329e90d863
Sieciech <wojciech.mocek@o2.pl>
2019-01-29 01:09:40 +0100

    added unit test

--- add_one/src/lib.rs
+++ add_one/src/lib.rs
@@ -4,6 +4,11 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn when_int_is_16_then_result_should_be_17() {
+        assert_eq!(17, super::add_one(16));
+    }
 }
 
 pub fn add_one(x: i32) -> i32 {
